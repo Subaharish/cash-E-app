3b7d8d42cddf33647cddaeb1b962986ef4ed752b
Harish <48942935+Subaharish@users.noreply.github.com>
2023-01-28 19:08:10 +0530

    added graphics

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// const card1 = document.querySelector('.card-1')
-// const cardb4 = document.querySelector('.card-b4')
-// const cardContainer = document.querySelector('.card-container')
+const card1 = document.querySelector('.card-1')
+const cardb4 = document.querySelector('.card-b4')
+const cardContainer = document.querySelector('.card-container')
 
-// cardb4.style.display = 'none'
+cardb4.style.display = 'none'
 
 // let startX, startY, moveX, moveY
 // cardContainer.addEventListener('touchstart', ()=>{
