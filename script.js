fdde9cbf5376d10f0ace2e32976e156c6b390484
Harish <48942935+Subaharish@users.noreply.github.com>
2023-01-28 19:02:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const card1 = document.querySelector('.card-1')
+// const cardb4 = document.querySelector('.card-b4')
+// const cardContainer = document.querySelector('.card-container')
+
+// cardb4.style.display = 'none'
+
+// let startX, startY, moveX, moveY
+// cardContainer.addEventListener('touchstart', ()=>{
+//     console.log("touch")
+// })
+
